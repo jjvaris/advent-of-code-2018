2702a70663cf3c51091835858a18401d97c2234a
Jouni Varis <jjvaris@gmail.com>
2018-12-06 15:41:18 +0200

    day 3.2

--- 3/advent3.js
+++ 3/advent3.js
@@ -1,6 +1,6 @@
 const Advent = require('../advent');
 
-class Advent2 extends Advent {
+class Advent3 extends Advent {
   constructor(day) {
     super(day);
   }
@@ -14,7 +14,7 @@ class Advent2 extends Advent {
       const [w, h] = size.split('x').map(number => parseInt(number));
       for (let i = y; i < y + h; i++) {
         for (let j = x; j < x + w; j++) {
-          const key = `${j} ${i}`;
+          const key = `${j},${i}`;
           if (!fabric[key]) fabric[key] = 0;
           fabric[key]++;
         }
@@ -24,8 +24,31 @@ class Advent2 extends Advent {
   }
 
   executeSecondAdvent() {
-    //const lines = super.getFileLines('input2.txt');
+    const lines = super.getFileLines('input.txt');
+    const fabric = {};
+    const claims = {};
+    lines.forEach(line => {
+      const id = line.split(' @ ')[0];
+      const [xy, size] = line.split(' @ ')[1].split(': ');
+      const [x, y] = xy.split(',').map(number => parseInt(number));
+      const [w, h] = size.split('x').map(number => parseInt(number));
+      for (let i = y; i < y + h; i++) {
+        for (let j = x; j < x + w; j++) {
+          const key = `${j},${i}`;
+          if (!fabric[key]) fabric[key] = { count: 0, ids: [] };
+          if (!claims[id]) claims[id] = 1;
+          fabric[key].count++;
+          fabric[key].ids.push(id);
+          if (fabric[key].count > 1) {
+            fabric[key].ids.forEach(id => {
+              claims[id]++;
+            });
+          }
+        }
+      }
+    });
+    return Object.entries(claims).find(([k, v]) => v === 1)[0];
   }
 }
 
-module.exports = Advent2;
+module.exports = Advent3;
